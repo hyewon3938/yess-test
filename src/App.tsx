f932ff3461dd46369e149096a72ec1130594697e
hyewon3938 <hyewon3938@naver.com>
2024-08-29 21:02:48 +0900

    [#1] chore : styled-components 설치 및 GlobalStyle 적용
    - styled-components 설치
    - GlobalStyle 적용을 위한 GlobalStyleProvider 컴포넌트 추가 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import GlobalStyleProvider from "./styles/GlobalStyleProvider";
 import CatViewer from "./CatViewer";
 import WorkingHours from "./WorkingHours";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-          }}
-        >
-          <ul>
-            <li>
-              <Link to="/cat-viewer">CatViewer</Link>
-            </li>
-            <li>
-              <Link to="/working-hour">WorkingHours</Link>
-            </li>
-          </ul>
-        </nav>
+    <GlobalStyleProvider>
+      <Router>
+        {/* <div className="App">
+          <nav
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+            }}
+          >
+            <ul>
+              <li>
+                <Link to="/cat-viewer">CatViewer</Link>
+              </li>
+              <li>
+                <Link to="/working-hour">WorkingHours</Link>
+              </li>
+            </ul>
+          </nav> */}
         <Switch>
-          <Route path="/cat-viewer">
-            <CatViewer />
-          </Route>
-          <Route path="/working-hour">
-            <WorkingHours />
-          </Route>
+          <Route path="/cat-viewer" component={CatViewer} />
+          <Route path="/working-hour" component={WorkingHours} />
         </Switch>
-      </div>
-    </Router>
+        {/* </div> */}
+      </Router>
+    </GlobalStyleProvider>
   );
 }
 
