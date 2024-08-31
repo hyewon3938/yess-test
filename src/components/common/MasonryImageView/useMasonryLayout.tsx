9f7430dbe0fead48b5f35a1eed9fd9f00cba01c1
hyewon3938 <hyewon3938@naver.com>
2024-08-31 13:53:59 +0900

    [#2] feat : 반응형 MasonryImageView 구현
    - 화면 너비에 따라 화면에 보여줄 column 개수 지정 로직 추가
    - column 개수에따라 이미지 재분배 로직 추가
    - 반응형 breakpoints 파일 추가

--- src/components/common/MasonryImageView/useMasonryLayout.tsx
+++ src/components/common/MasonryImageView/useMasonryLayout.tsx
@@ -1,63 +1,84 @@
-import { useState, useRef, useCallback } from "react";
+import { useEffect, useMemo, useState, useCallback, RefObject } from "react";
 import { ImageData } from "../../CatViewer/catViewer.types";
+import { device } from "../../../styles/breakpoints";
+import debounce from "lodash/debounce";
 
-const useMasonryLayout = (columnCount: number = 3) => {
-  const [columns, setColumns] = useState<Array<ImageData[]>>(
-    Array(columnCount).fill([])
+const useMasonryLayout = (wrapRef: RefObject<HTMLDivElement>) => {
+  const [columns, setColumns] = useState<Array<ImageData[]>>([]);
+  const [columnCount, setColumnCount] = useState<number>(3);
+
+  const calculateColumnCount = useCallback(() => {
+    if (window.innerWidth >= device.mobile) return 3;
+    if (window.innerWidth >= device.small) return 2;
+    return 1;
+  }, []);
+
+  const handleResize = useCallback(() => {
+    const newColumnCount = calculateColumnCount();
+    if (newColumnCount !== columnCount) {
+      setColumnCount(newColumnCount);
+    }
+  }, [calculateColumnCount, columnCount]);
+
+  const debouncedHandleResize = useMemo(
+    () => debounce(handleResize, 200),
+    [handleResize]
   );
-  const [columnHeights, setColumnHeights] = useState<number[]>(
-    Array(columnCount).fill(0)
-  );
-  const columnRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    handleResize(); // 초기 렌더링 시 column 개수 설정
+    window.addEventListener("resize", debouncedHandleResize);
+    return () => window.removeEventListener("resize", debouncedHandleResize);
+  }, [debouncedHandleResize, handleResize]);
 
   const calculateExpectedHeight = useCallback(
-    (originalWidth: number, originalHeight: number) => {
-      const columnWidth: number = columnRef.current?.offsetWidth || 0;
+    (columnWidth: number, originalWidth: number, originalHeight: number) => {
       const scaleFactor: number = columnWidth / originalWidth;
+      // 데이터가 없을 경우를 대비해 임의로 column의 너비 값을 넣어줌
       return originalHeight * scaleFactor || columnWidth;
     },
     []
   );
 
+  // 이미지를 컬럼에 분배하는 함수
   const distributeImages = useCallback(
-    (newImages: ImageData[]) => {
-      setColumns((prevColumns) => {
-        const updatedColumns = prevColumns.map((column) => [...column]);
-        const updatedColumnHeights = [...columnHeights];
+    (images: ImageData[]) => {
+      if (!wrapRef?.current) return;
 
-        newImages.forEach((image, index) => {
-          const expectedHeight = calculateExpectedHeight(
-            image.width,
-            image.height
-          );
-          const newImage = {
-            ...image,
-            order: index,
-            expectedHeight: expectedHeight,
-          };
-          const minHeightIndex = updatedColumnHeights.indexOf(
-            Math.min(...updatedColumnHeights)
-          );
+      const updatedColumns = Array.from(
+        { length: columnCount },
+        () => [] as ImageData[]
+      );
+      const columnHeights = Array(columnCount).fill(0);
+      const columnWidth: number =
+        (wrapRef.current?.offsetWidth - (columnCount - 1 * 16)) / 3;
 
-          updatedColumns[minHeightIndex].push(newImage);
-          updatedColumnHeights[minHeightIndex] += expectedHeight;
+      images.forEach((image, index) => {
+        const shortestColumnIndex = columnHeights.indexOf(
+          Math.min(...columnHeights)
+        );
+
+        const expectedHeight = calculateExpectedHeight(
+          columnWidth,
+          image.width,
+          image.height
+        );
+
+        updatedColumns[shortestColumnIndex].push({
+          ...image,
+          order: index,
+          expectedHeight: expectedHeight,
         });
 
-        setColumnHeights(updatedColumnHeights);
-        return updatedColumns;
+        columnHeights[shortestColumnIndex] += expectedHeight;
       });
+
+      setColumns(updatedColumns);
     },
-    [calculateExpectedHeight, columnHeights]
+    [calculateExpectedHeight, wrapRef, columnCount]
   );
 
-  const addImages = useCallback(
-    (newImages: ImageData[]) => {
-      distributeImages(newImages);
-    },
-    [distributeImages]
-  );
-
-  return { columns, columnRef, addImages };
+  return { columns, distributeImages };
 };
 
 export default useMasonryLayout;
